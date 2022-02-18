b9ac09c117611e3e2e4897c69c78bc773df7e6ad
d00m4n <d00m4n@debian01.d00m4n.net>
2022-02-18 11:13:32 +0100

    initial realese

---
+++ setup.sh
@@ -0,0 +1,10 @@
+repos="majord00m tuitBot tabout bittrexBot cmcairbot dtools dropBot "
+path="../bots"
+for repo in $repos
+do
+    echo "$path/$repo"
+    git clone  --progress git@github.com:d00m4n/$repo.git "$path/$repo"
+done
+
+# git clone git@github.com:d00m4n/majord00m.git ../majord00m
+# git clone git@github.com:d00m4n/tuitBot.git
