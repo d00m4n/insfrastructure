d678dcf8554fdee8c185e950a5f10e0b65863a16
root <root@debian.d00m4n.net>
2023-01-20 06:12:25 +0100

    added apts and user to sudoers group

--- setup.sh
+++ setup.sh
@@ -1,4 +1,27 @@
-apt install docker sudo -y
+user="$1"
+if [ ! -n "$user" ]
+then
+	echo "Please specify user"
+	exit
+else
+	echo "User:" $user
+fi
+
+sudo apt-get install -y ca-certificates curl gnupg lsb-release sudo
+echo "Adding dockers keys"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "Setup repository"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo Addind $user to Sudors grop
+usermod -a -G sudo $user
+
+exit
+
 
 repos="majord00m tuitBot tabout bittrexBot cmcairbot dtools dropBot "
 path="../bots"
